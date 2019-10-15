b4cd2761f9c46cc60366d6bff068eee7b5197eb4
Your Name <alexandre.dalabona@gmail.com>
2019-10-14 23:49:25 -0300

    Ambientes e conceitos

--- index.js
+++ index.js
@@ -5,17 +5,44 @@ server.use(express.json());
 
 const users = ['Alexandre', 'Ariane', 'Dougas,', 'Maria', 'Mariela'];
 
+server.use((req, res, next) => {
+	next();
+});
+
+function checkUserExists(req, res, next) {
+	if (!req.body.name) {
+		return res.status(400).json({
+			error: 'Usuário não encontro no body'
+		})
+	}
+
+	return next();
+}
+
+function checkUserInArray(req, res, next) {
+	const { index } = req.params;
+	const user = users[index];
+
+	if (!user) {
+		return res.status(400).json({
+			error: 'Usuário não existe'
+		});
+	}
+
+	req.user = user;
+
+	return next();
+}
+
 server.get('/users', ({res}) => {
     res.json(users);
 });
 
-server.get('/users/:index', (req, res) => {
-    const { index } = req.params;
-    
-    res.json(users[index]);
+server.get('/users/:index', checkUserInArray, (req, res) => {    
+    res.json(req.user);
 });
 
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExists, (req, res) => {
     const { name } = req.body;
    
     users.push(name);
@@ -23,7 +50,7 @@ server.post('/users', (req, res) => {
     res.json(users);
 });
 
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUserInArray, checkUserExists, (req, res) => {
     const { index } = req.params;
     const { name } = req.body;
     
@@ -32,7 +59,7 @@ server.put('/users/:index', (req, res) => {
     res.json(users);
 });
 
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index', checkUserInArray, (req, res) => {
     const { index } = req.params;
     
     users.splice(index, 1);
